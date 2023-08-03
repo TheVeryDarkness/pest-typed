937d14705416aaf3d6d485bb7daaa807ca828bd6
博奕 黄 <3266343194@qq.com>
2023-08-03 11:04:38 +0800

    Update tests on Pair/Pairs.

--- derive/tests/pairs.rs
+++ derive/tests/pairs.rs
@@ -8,7 +8,9 @@
 // modified, or distributed except according to those terms.
 
 extern crate alloc;
-use pest_typed::{error::Error, ParsableTypedNode};
+use std::fmt::Write;
+
+use pest_typed::{error::Error, iterators::PairTree, ParsableTypedNode};
 use pest_typed_derive::TypedParser;
 
 #[derive(TypedParser)]
@@ -18,7 +20,40 @@ struct CSV;
 
 #[test]
 fn pairs() -> Result<(), Error<Rule>> {
-    let _res = pairs::file::parse("a,b,c\nd,e,f")?;
-    // Not finished.
+    let res = pairs::file::parse("a,b,c\nd,e,f")?;
+    let mut buf = String::new();
+    res.iterate_pre_order(|pair, depth| {
+        write!(&mut buf, "{}", "  ".repeat(depth)).unwrap();
+        writeln!(&mut buf, "{:?} {:?}", pair.rule(), pair.span().as_str()).unwrap();
+    });
+    assert_eq!(
+        buf,
+        r#"file "a,b,c\nd,e,f"
+  row "a,b,c"
+    item "a"
+      non_escaped_item "a"
+        legal_ascii "a"
+    comma ","
+    item "b"
+      non_escaped_item "b"
+        legal_ascii "b"
+    comma ","
+    item "c"
+      non_escaped_item "c"
+        legal_ascii "c"
+  row "d,e,f"
+    item "d"
+      non_escaped_item "d"
+        legal_ascii "d"
+    comma ","
+    item "e"
+      non_escaped_item "e"
+        legal_ascii "e"
+    comma ","
+    item "f"
+      non_escaped_item "f"
+        legal_ascii "f"
+"#
+    );
     Ok(())
 }
